4e1e3ee45746886acccbd792b8a26dde3072803f
butyeser <572984581@qq.com>
2020-02-21 01:57:26 +0800

    update
    update

--- Mysql基础知识.sql
+++ Mysql基础知识.sql
@@ -1,3 +1,96 @@
+# 20200221
+
+
+# 1 汇总函数
+# 查询确诊人数按城市排名
+select 
+cityname,
+sum(people) 
+from citytable 
+group by city 
+order by cityname desc;
+
+select 城市,sum(确诊人数) from city group by 城市 order by sum(确诊人数) desc;
+
+
+# 2 子查询
+# 什么是子查询？子查询就是嵌套在主查询中的查询
+# 主表中的字段和子表的字有相同字段，才能进行关联子查询
+# 在SELECT中嵌套
+# 相关子查询
+# 外部查询返回  一行 ，子查询就执行一次
+SELECT s.student_id,s.student_name,(SELECT class_name FROM t_class c WHERE c.class_id=s.class_id) FROM t_student s GROUP BY s.student_id;  
+
+
+# 3 非相关子查询-查出所有值做判断
+# 在WHERE中嵌套
+# ALL运算符和子查询的结果逐一比较，必须全部满足时表达式的值才为真
+# 子查询总共执行一次，执行完毕后后将值传递给外部查询
+SELECT * FROM t_student 
+WHERE student_subject='C语言' 
+AND student_score>=ALL(SELECT student_score FROM t_student WHERE student_subject='C语言') ;
+
+SELECT * FROM t_student 
+WHERE student_subject='C语言' 
+AND student_score >(SELECT student_score FROM t_student WHERE student_name='李四' AND student_subject='C语言'); 
+
+SELECT * FROM t_student s1 
+WHERE s1.student_score >= ALL(SELECT s2.student_score FROM t_student s2 WHERE s1.`student_subject`=s2.student_subjec
+
+select * from table_a where a = all(select a from table_b)
+
+select a from table_b
+
+select * from table_a where a = 1
+
+
+# 4.1 条件判断-case when else end
+
+select 
+users.name,
+(case users.sex when 1 then '男' when 2 then '女' else 0 end ) as sex
+from users;
+
+select u_id , u_name , u_score ,
+case when u_score >= 65.45 then '优秀'
+when u_score >=60 and u_score<85 then '及格'
+when u_score <60 then '不及格'
+else '数据错误'
+end 
+from T_USER ;
+
+
+# 4.2 条件判断+求和 sum(case when)
+# 求男的多少个，女的多少个，空值多少个人
+select 
+sum(case users.sex when '男' then 1 else 0 end),
+sum(case users.sex when '女' then 1 else 0 end),
+sum(case users.sex when '空的' then 1 else 0 end)
+from users;
+
+
+# 5 汇总函数-count-sum-avg-max-min
+
+# AVG()：返回某列的平均值
+SELECT AVG(price) AS 平均价格 FROM products WHERE id = 1003;
+
+# COUNT()：返回某列的行数
+SELECT COUNT(id) AS 行数 FROM products;
+SELECT COUNT(*) AS 行数 FROM products;
+
+# MAX()：返回某列的最大值
+SELECT MAX(price) AS 最大值 FROM products;
+
+# MIN()：返回某列的最小值
+SELECT MIN(price) AS 最小值 FROM products;
+
+# SUM()：返回某列值之和
+SELECT SUM(price) FROM products WHERE id = 1002;
+SELECT SUM(price * price_num) FROM products WHERE id = 1002;
+
+
+
+
 # 20200215
 
 
